850e75a739ba2e9bbeb90d07478ba4d97556362d
Lyndis Mushin <alestjoh@gmail.com>
2019-04-30 15:20:06 -0400

    Retrieve data from Api in viewModel

--- app/src/main/java/com/example/moviesearcher/viewModel/MainViewModel.java
+++ app/src/main/java/com/example/moviesearcher/viewModel/MainViewModel.java
@@ -1,17 +1,26 @@
 package com.example.moviesearcher.viewModel;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import com.example.moviesearcher.model.MovieApi;
 import com.example.moviesearcher.model.MovieApiData;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainViewModel extends ViewModel {
     private MovieApi api;
+
     private MutableLiveData<MovieApiData> movieData = new MutableLiveData<>();
+
+    public LiveData<MovieApiData> getMovieData() {
+        return movieData;
+    }
 
     private void initRetrofit() {
         api = new Retrofit.Builder()
@@ -21,10 +30,25 @@ public class MainViewModel extends ViewModel {
                 .create(MovieApi.class);
     }
 
-    private void retreiveDataFromApi() {
+    private void retrieveDataFromApi() {
         if (api == null) {
             initRetrofit();
         }
+        api.getAllMovies().enqueue(new Callback<MovieApiData>() {
+            @Override
+            public void onResponse(Call<MovieApiData> call, Response<MovieApiData> response) {
+                movieData.postValue(response.body());
+            }
 
+            @Override
+            public void onFailure(Call<MovieApiData> call, Throwable t) {
+                movieData.postValue(null);
+            }
+        });
+    }
+
+    public void retrieveData() {
+        //TODO: determine whether to get data from Retrofit or Room
+        retrieveDataFromApi();
     }
 }
