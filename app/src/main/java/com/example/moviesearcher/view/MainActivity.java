acab50bc016a00ef6d7e273550d9b01eedc7687d
Lyndis Mushin <alestjoh@gmail.com>
2019-04-30 15:26:34 -0400

    Refactor TextWatcher methods in MainActivity

--- app/src/main/java/com/example/moviesearcher/view/MainActivity.java
+++ app/src/main/java/com/example/moviesearcher/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.moviesearcher.view;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -8,16 +9,19 @@ import android.text.TextWatcher;
 import android.widget.EditText;
 
 import com.example.moviesearcher.R;
+import com.example.moviesearcher.viewModel.MainViewModel;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
 
     @BindView(R.id.et_search_main)
     public EditText search;
     @BindView(R.id.recycler_view_main)
     public RecyclerView recyclerView;
+
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +30,23 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        search.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) { }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                //TODO: update recyclerView with new filter
-            }
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        viewModel.getMovieData().observe(this, data -> {
+            //TODO: update recyclerView with new data
         });
+        viewModel.retrieveData();
+
+        search.addTextChangedListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        //TODO: update recyclerView with new filter through viewModel
     }
 }
