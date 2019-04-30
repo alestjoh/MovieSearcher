c621ff5bb16804e4d7c149d6cb0f14ea1a562a59
Lyndis Mushin <alestjoh@gmail.com>
2019-04-30 15:13:31 -0400

    Remove Dagger dependency, add RxJava

--- app/src/main/java/com/example/moviesearcher/viewModel/MainViewModel.java
+++ app/src/main/java/com/example/moviesearcher/viewModel/MainViewModel.java
@@ -1,4 +1,30 @@
 package com.example.moviesearcher.viewModel;
 
-public class MainViewModel {
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.moviesearcher.model.MovieApi;
+import com.example.moviesearcher.model.MovieApiData;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainViewModel extends ViewModel {
+    private MovieApi api;
+    private MutableLiveData<MovieApiData> movieData = new MutableLiveData<>();
+
+    private void initRetrofit() {
+        api = new Retrofit.Builder()
+                .baseUrl("https://movies-sample.herokuapp.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(MovieApi.class);
+    }
+
+    private void retreiveDataFromApi() {
+        if (api == null) {
+            initRetrofit();
+        }
+
+    }
 }
